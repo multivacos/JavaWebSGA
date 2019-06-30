d738bc3d5aa88827d702bfe638d938b68be6ef4b
Jonatan <alvarez.geek@hotmail.com>
2019-06-30 09:20:18 -0500

    Mantenimiento de proveedor

---
+++ build/web/view/proveedor/listaProveedor.jsp
@@ -0,0 +1,127 @@
+<%-- 
+    Document   : listaAlumno
+    Created on : 29/06/2019, 02:58:58 PM
+    Author     : Jonatan
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<%@page import="entity.Proveedor" %>
+<%@page import="java.util.List" %>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Proveedor</title>
+    </head>
+    <body>
+    <script src="js/proveedor.js"></script>
+    <section class="content-header">
+        <h1>
+            Proveedor <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Nuevo</button>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Logística</li>
+        </ol>
+    </section>
+ <section class="content">
+    <div class="row">
+        <div class="col-xs-12">
+        <div class="box">
+        <div class="box-body">
+        
+        <br>
+        <table class="table table-bordered table-hover">
+            
+            <tr class="grilla_cabecera">
+                <th>Id</th><th>Ruc</th><th>Razón social</th><th>Teléfono</th><th>Dirección</th><th>Eliminar</th><th>Editar</th>
+            </tr>
+            
+            <% 
+                List<Proveedor> data = (List<Proveedor>)request.getAttribute("data");
+                if (data!=null) {
+                        for(Proveedor x: data){
+                        %>
+                        <tr class="grilla_campo">
+                            <td><%= x.getIdProveedor()%></td>
+                            <td><%= x.getRuc()%></td>
+                            <td><%= x.getRazonSocial()%></td>
+                            <td><%= x.getTelefono()%></td>
+                            <td><%= x.getDireccion()%></td>
+                            <td>
+                                <a href="proveedor?metodo=elimina&id=<%= x.getIdProveedor()%>">
+                                    <img alt="Elimina" src="images/Delete.gif">
+                                </a>
+                            </td>
+                            <td>
+                                <a href="proveedor?metodo=busca&id=<%= x.getIdProveedor()%>">
+                                    <img alt="Actualiza" src="images/Edit.gif">
+                                </a>
+                            </td>
+                        </tr>
+                        <%
+                        }
+                    }
+            %>
+        </table>
+        </div>
+        </div>
+        </div>
+    </div>
+ </section>
+       
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Insertar nuevo proveedor</h4>
+        </div>
+        <div class="modal-body">
+          <form action="proveedor" method="post" class="form-horizontal">
+            <input type="hidden" name="metodo" value="registra">
+            <div class="box-body">
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">RUC</label>
+
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" maxlength="11" name="ruc" placeholder="Inserta el numero de RUC" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">RAZON SOCIAL</label>
+
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="razonSocial" placeholder="Inserta la razon social" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">TELEFONO</label>
+
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" maxlength="9" name="telefono" placeholder="Inserta el telefono" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">DIRECCION</label>
+
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="direccion" placeholder="Inserta la direccion" required>
+                    </div>
+                </div>          
+                <div class="modal-footer">
+                  <button type="submit" class="btn btn-success">Guardar</button>
+                  <button type="reset" class="btn btn-warning">Limpiar</button>
+                </div>
+        </form>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  </div>
+    </body>
+</html>
--- script_bd/dump-bdsga.sql
+++ script_bd/dump-bdsga.sql
@@ -1,78 +1,57 @@
--- MySQL dump 10.16  Distrib 10.1.25-MariaDB, for Win32 (AMD64)
---
--- Host: localhost    Database: bdsga
--- ------------------------------------------------------
--- Server version	10.1.25-MariaDB
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.3.16-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             10.2.0.5599
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `almacen`
---
 
-DROP TABLE IF EXISTS `almacen`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `almacen` (
+-- Volcando estructura de base de datos para bdsga
+DROP DATABASE IF EXISTS `bdsga`;
+CREATE DATABASE IF NOT EXISTS `bdsga` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `bdsga`;
+
+-- Volcando estructura para tabla bdsga.almacen
+CREATE TABLE IF NOT EXISTS `almacen` (
   `id_almacen` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
   `direccion` varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`id_almacen`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `almacen`
---
-
-LOCK TABLES `almacen` WRITE;
+-- Volcando datos para la tabla bdsga.almacen: ~1 rows (aproximadamente)
 /*!40000 ALTER TABLE `almacen` DISABLE KEYS */;
-INSERT INTO `almacen` VALUES (1,'Almacen Central','Av. lima 451');
+INSERT INTO `almacen` (`id_almacen`, `descripcion`, `direccion`) VALUES
+	(1, 'Almacen Central', 'Av. lima 451');
 /*!40000 ALTER TABLE `almacen` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `clase`
---
-
-DROP TABLE IF EXISTS `clase`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `clase` (
+-- Volcando estructura para tabla bdsga.clase
+CREATE TABLE IF NOT EXISTS `clase` (
   `id_clase` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
   `id_clase_padre` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_clase`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `clase`
---
-
-LOCK TABLES `clase` WRITE;
+-- Volcando datos para la tabla bdsga.clase: ~6 rows (aproximadamente)
 /*!40000 ALTER TABLE `clase` DISABLE KEYS */;
-INSERT INTO `clase` VALUES (1,'Monturas',NULL),(2,'Lunas',NULL),(3,'Lentes para hombre',NULL),(4,'Lentes para mujer',NULL),(5,'Lentes para niños',NULL),(6,'Lentes de contacto',NULL);
+INSERT INTO `clase` (`id_clase`, `descripcion`, `id_clase_padre`) VALUES
+	(1, 'Monturas', NULL),
+	(2, 'Lunas', NULL),
+	(3, 'Lentes para hombre', NULL),
+	(4, 'Lentes para mujer', NULL),
+	(5, 'Lentes para niños', NULL),
+	(6, 'Lentes de contacto', NULL);
 /*!40000 ALTER TABLE `clase` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `movimiento_producto`
---
-
-DROP TABLE IF EXISTS `movimiento_producto`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `movimiento_producto` (
+-- Volcando estructura para tabla bdsga.movimiento_producto
+CREATE TABLE IF NOT EXISTS `movimiento_producto` (
   `id_mov_prod` int(11) NOT NULL AUTO_INCREMENT,
   `fecha_reg` datetime DEFAULT NULL,
   `serie` varchar(5) COLLATE utf8_spanish_ci DEFAULT NULL,
@@ -94,75 +73,45 @@ CREATE TABLE `movimiento_producto` (
   CONSTRAINT `movimiento_producto_fk_2` FOREIGN KEY (`id_tipo_mov`) REFERENCES `tipo_movimiento` (`id_tipo_mov`),
   CONSTRAINT `movimiento_producto_fk_3` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id_proveedor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `movimiento_producto`
---
-
-LOCK TABLES `movimiento_producto` WRITE;
+-- Volcando datos para la tabla bdsga.movimiento_producto: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `movimiento_producto` DISABLE KEYS */;
 /*!40000 ALTER TABLE `movimiento_producto` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `opcion`
---
-
-DROP TABLE IF EXISTS `opcion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `opcion` (
+-- Volcando estructura para tabla bdsga.opcion
+CREATE TABLE IF NOT EXISTS `opcion` (
   `id_opcion` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(50) COLLATE utf8_spanish_ci DEFAULT NULL,
   `url` varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
   `orden` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_opcion`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `opcion`
---
-
-LOCK TABLES `opcion` WRITE;
+-- Volcando datos para la tabla bdsga.opcion: ~5 rows (aproximadamente)
 /*!40000 ALTER TABLE `opcion` DISABLE KEYS */;
-INSERT INTO `opcion` VALUES (1,'Proveedor','url1',1),(2,'Producto','url2',2),(3,'Almacen',NULL,3),(4,'Parte De Entrada',NULL,4),(5,'Guia De Remisión',NULL,5);
+INSERT INTO `opcion` (`id_opcion`, `descripcion`, `url`, `orden`) VALUES
+	(1, 'Proveedor', 'url1', 1),
+	(2, 'Producto', 'url2', 2),
+	(3, 'Almacen', NULL, 3),
+	(4, 'Parte De Entrada', NULL, 4),
+	(5, 'Guia De Remisión', NULL, 5);
 /*!40000 ALTER TABLE `opcion` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `perfil`
---
-
-DROP TABLE IF EXISTS `perfil`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `perfil` (
+-- Volcando estructura para tabla bdsga.perfil
+CREATE TABLE IF NOT EXISTS `perfil` (
   `id_perfil` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(50) COLLATE utf8_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`id_perfil`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `perfil`
---
-
-LOCK TABLES `perfil` WRITE;
+-- Volcando datos para la tabla bdsga.perfil: ~1 rows (aproximadamente)
 /*!40000 ALTER TABLE `perfil` DISABLE KEYS */;
-INSERT INTO `perfil` VALUES (1,'Administrador');
+INSERT INTO `perfil` (`id_perfil`, `descripcion`) VALUES
+	(1, 'Administrador');
 /*!40000 ALTER TABLE `perfil` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `perfil_opcion`
---
-
-DROP TABLE IF EXISTS `perfil_opcion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `perfil_opcion` (
+-- Volcando estructura para tabla bdsga.perfil_opcion
+CREATE TABLE IF NOT EXISTS `perfil_opcion` (
   `id_perfil_opcion` int(11) NOT NULL AUTO_INCREMENT,
   `id_perfil` int(11) DEFAULT NULL,
   `id_opcion` int(11) DEFAULT NULL,
@@ -172,26 +121,16 @@ CREATE TABLE `perfil_opcion` (
   CONSTRAINT `perfil_opcion_fk` FOREIGN KEY (`id_perfil`) REFERENCES `perfil` (`id_perfil`),
   CONSTRAINT `perfil_opcion_fk_2` FOREIGN KEY (`id_opcion`) REFERENCES `opcion` (`id_opcion`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `perfil_opcion`
---
-
-LOCK TABLES `perfil_opcion` WRITE;
+-- Volcando datos para la tabla bdsga.perfil_opcion: ~2 rows (aproximadamente)
 /*!40000 ALTER TABLE `perfil_opcion` DISABLE KEYS */;
-INSERT INTO `perfil_opcion` VALUES (1,1,1),(2,1,2);
+INSERT INTO `perfil_opcion` (`id_perfil_opcion`, `id_perfil`, `id_opcion`) VALUES
+	(1, 1, 1),
+	(2, 1, 2);
 /*!40000 ALTER TABLE `perfil_opcion` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `producto`
---
-
-DROP TABLE IF EXISTS `producto`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `producto` (
+-- Volcando estructura para tabla bdsga.producto
+CREATE TABLE IF NOT EXISTS `producto` (
   `id_producto` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
   `tamanio` varchar(50) COLLATE utf8_spanish_ci DEFAULT NULL,
@@ -202,77 +141,60 @@ CREATE TABLE `producto` (
   PRIMARY KEY (`id_producto`),
   KEY `producto_fk` (`id_clase`),
   CONSTRAINT `producto_fk` FOREIGN KEY (`id_clase`) REFERENCES `clase` (`id_clase`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
---
--- Dumping data for table `producto`
---
-
-LOCK TABLES `producto` WRITE;
+-- Volcando datos para la tabla bdsga.producto: ~6 rows (aproximadamente)
 /*!40000 ALTER TABLE `producto` DISABLE KEYS */;
-INSERT INTO `producto` VALUES (26,'ARNETTE','','','CUADRADO','ACETATO',3),(27,'OAKLEY','','','RECTANGULAR','NYLON PROPIONATO',3),(28,'ARMANI EXCHANGE','','','RECTANGULAR','NYLON PROPIONATO',3),(29,'POLO RL','','','RECTANGULAR','NYLON PROPIONATO',4),(30,'PRADA','','','CIRCULAR','ACERO',4),(31,'RAY BAN','','','CUADRADO','NYLON',5);
+INSERT INTO `producto` (`id_producto`, `descripcion`, `tamanio`, `color`, `forma`, `material`, `id_clase`) VALUES
+	(26, 'ARNETTE', '14 CM', 'YELOOW', 'CUADRADO', 'ACETATO', 3),
+	(27, 'OAKLEY', '', '', 'RECTANGULAR', 'NYLON PROPIONATO', 3),
+	(28, 'ARMANI EXCHANGE', '', '', 'RECTANGULAR', 'NYLON PROPIONATO', 3),
+	(29, 'POLO RL', '', '', 'RECTANGULAR', 'NYLON PROPIONATO', 4),
+	(30, 'PRADA', '', '', 'CIRCULAR', 'ACERO', 4),
+	(31, 'RAY BAN', '', '', 'CUADRADO', 'NYLON', 5);
 /*!40000 ALTER TABLE `producto` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `proveedor`
---
-
-DROP TABLE IF EXISTS `proveedor`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `proveedor` (
+-- Volcando estructura para tabla bdsga.proveedor
+CREATE TABLE IF NOT EXISTS `proveedor` (
   `id_proveedor` int(11) NOT NULL AUTO_INCREMENT,
   `ruc` varchar(11) COLLATE utf8_spanish_ci DEFAULT NULL,
   `razon_social` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
   `telefono` varchar(15) COLLATE utf8_spanish_ci DEFAULT NULL,
   `direccion` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`id_proveedor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
---
--- Dumping data for table `proveedor`
---
-
-LOCK TABLES `proveedor` WRITE;
+-- Volcando datos para la tabla bdsga.proveedor: ~10 rows (aproximadamente)
 /*!40000 ALTER TABLE `proveedor` DISABLE KEYS */;
+INSERT INTO `proveedor` (`id_proveedor`, `ruc`, `razon_social`, `telefono`, `direccion`) VALUES
+	(1, '10123123128', 'Resinas del Perú', '999888777', 'Jr La curva 882 4to piso Lince'),
+	(2, '10656463637', 'Plasticos la colonia', '999777555', 'Jr los pinos 123'),
+	(3, '1045554647', 'Importaciones la vela', '987564321', 'Jr nogales 12345'),
+	(4, '1045554654', 'Importadora general del norte', '999564321', 'Jr cipreses 231'),
+	(5, '1045554687', 'Importaciones el olivar', '987564321', 'Jr la cumbre 635'),
+	(6, '1045554632', 'Importaciones lider', '955564111', 'Jr jorge chavez 876'),
+	(7, '1045554690', 'Importaciones espectrum', '944564345', 'Jr polonia 876'),
+	(8, '1045554632', 'Importadora lentes y lentas', '912564452', 'Jr san martion de porres 654'),
+	(9, '1045554621', 'Importadora la solucion', '954564987', 'Jr canova 2312'),
+	(10, '1045554611', 'Importadora el risco', '912564789', 'Jr frans hals 333');
 /*!40000 ALTER TABLE `proveedor` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `tipo_movimiento`
---
-
-DROP TABLE IF EXISTS `tipo_movimiento`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tipo_movimiento` (
+-- Volcando estructura para tabla bdsga.tipo_movimiento
+CREATE TABLE IF NOT EXISTS `tipo_movimiento` (
   `id_tipo_mov` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`id_tipo_mov`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `tipo_movimiento`
---
-
-LOCK TABLES `tipo_movimiento` WRITE;
+-- Volcando datos para la tabla bdsga.tipo_movimiento: ~2 rows (aproximadamente)
 /*!40000 ALTER TABLE `tipo_movimiento` DISABLE KEYS */;
-INSERT INTO `tipo_movimiento` VALUES (1,'Entrada'),(2,'Salida');
+INSERT INTO `tipo_movimiento` (`id_tipo_mov`, `descripcion`) VALUES
+	(1, 'Entrada'),
+	(2, 'Salida');
 /*!40000 ALTER TABLE `tipo_movimiento` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `usuario`
---
-
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `usuario` (
+-- Volcando estructura para tabla bdsga.usuario
+CREATE TABLE IF NOT EXISTS `usuario` (
   `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
   `usuario` varchar(15) COLLATE utf8_spanish_ci DEFAULT NULL,
   `clave` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
@@ -282,29 +204,13 @@ CREATE TABLE `usuario` (
   KEY `usuario_fk` (`id_perfil`),
   CONSTRAINT `usuario_fk` FOREIGN KEY (`id_perfil`) REFERENCES `perfil` (`id_perfil`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
+-- Volcando datos para la tabla bdsga.usuario: ~1 rows (aproximadamente)
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'RMAQUERA','123456','',1);
+INSERT INTO `usuario` (`id_usuario`, `usuario`, `clave`, `estado`, `id_perfil`) VALUES
+	(1, 'RMAQUERA', '123456', b'1', 1);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Dumping routines for database 'bdsga'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-06-28 21:56:51
